40cf6863c8bfe3d855725b480951e8d165b84a4a
phong-phung-phinh <vingazs042@gmail.com>
2022-07-04 21:14:02 +0700

    update

--- db.js
+++ db.js
@@ -1,13 +1,8 @@
-const items = require("./data/items");
-const tags = require("./data/tags");
-const brands = require("./data/brands");
-const itemTypes = require("./data/itemTypes");
+const vani_key = require("./data/vani_key.json");
+
 
 module.exports = {
-  items,
-  tags,
-  brands,
-  itemTypes
+  vani_key
 }
 
 
